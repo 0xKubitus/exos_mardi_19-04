3ec2b7cf05ff080bb010104d99c9c2b380b8f795
0xKubitus <n.cubilier@gmail.com>
2022-04-19 16:17:33 +0200

    exo1 multiples - terminé !

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -12,4 +12,30 @@ end
 
 def sum_of_3_or_5_multiples(final_number)
 
+  # afficher alerte en cas d'input != integer
+  unless final_number.class == Integer && final_number >= 0
+    return "ALERTE GENERALE ! Pas d'integer !!!"
+  else
+    final_sum = 0 #cette variable est la somme finale qui sera retournée. Ici on l'initialise à zéro
+   
+    # Ici, lance une boucle qui va de zéro au chiffre "final_number" EXCLU (car on est sur du strictement inférieur)
+    for i in 0...final_number
+
+    # cette boucle indente à chaque tour une variable (par exemple "current_i_value") qui vaudra 0 puis 1 puis 2 ... etc.
+      current_i_value = i
+  
+      if is_multiple_of_3_or_5?(current_i_value)   
+        final_sum += current_i_value
+      end
+    end
+
+    if final_sum != nil
+      return final_sum
+    end
+
+  end 
+
 end
+
+  
+ 
--- spec/multiples_spec.rb
+++ spec/multiples_spec.rb
@@ -14,3 +14,18 @@ describe "the is_multiple_of_3_or_5? method" do
     expect(is_multiple_of_3_or_5?(4)).to eq(false)
   end
 end
+
+describe "the sum_of_3_or_5_multiples method" do
+  it "should return a result if the input in an Integer, even if 0" do
+    expect(sum_of_3_or_5_multiples(10)).to eq(23)
+    expect(sum_of_3_or_5_multiples(11)).to eq(33)
+    expect(sum_of_3_or_5_multiples(0)).to eq(0)
+    expect(sum_of_3_or_5_multiples(3)).to eq(0)
+  end
+
+  it "should return a warning when the input is not an integer" do
+    expect(sum_of_3_or_5_multiples(-1)).to eq("ALERTE GENERALE ! Pas d'integer !!!")
+    expect(sum_of_3_or_5_multiples(1.23)).to eq("ALERTE GENERALE ! Pas d'integer !!!")
+    expect(sum_of_3_or_5_multiples("chiffres")).to eq("ALERTE GENERALE ! Pas d'integer !!!")
+  end
+end
