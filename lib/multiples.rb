107957a821142f3dfa8ee9f60731f1b1da4bd780
0xKubitus <n.cubilier@gmail.com>
2022-04-19 14:28:47 +0200

    is_multiple_of_3_or_5?(n) method = OK

---
+++ lib/multiples.rb
@@ -0,0 +1,15 @@
+def is_multiple_of_3_or_5?(n)
+  n%3 == 0 || n%5 == 0 ? true : false
+end
+
+# p is_multiple_of_3_or_5?(3)
+# p is_multiple_of_3_or_5?(5)
+# p is_multiple_of_3_or_5?(51)
+# p is_multiple_of_3_or_5?(45)
+# p is_multiple_of_3_or_5?(2)
+# p is_multiple_of_3_or_5?(7)
+# p is_multiple_of_3_or_5?(4)
+
+def sum_of_3_or_5_multiples(final_number)
+
+end
